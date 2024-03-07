c3bb88771edcf630dc4225621a233cb2511c4b3a
Chit San Ko <105967028+Chitsk@users.noreply.github.com>
2024-03-07 15:04:45 +0630

    uploaded zabbix and phpipam setup scripts

---
+++ phpipam final.sh
@@ -0,0 +1,76 @@
+
+#PHPIPAM default login
+# username : admin
+# Password: ipamadmin
+
+#reference https://computingforgeeks.com/install-and-configure-phpipam-on-ubuntu-debian-linux/
+DATABASE_PASS='Pass-2024'
+sudo apt update
+sud apt install -y curl git vim
+
+sudo apt install mariadb-server mariadb-client -y
+sudo systemctl enable mariadb
+sudo systemctl start mariadb
+
+sudo mysqladmin -u root password "$DATABASE_PASS"
+sudo mysql -u root -p"$DATABASE_PASS" -e "CREATE DATABASE phpipam"
+sudo mysql -u root -p"$DATABASE_PASS" -e "GRANT ALL ON phpipam.* TO phpipam@localhost IDENTIFIED BY 'Pass-2024'"
+sudo mysql -u root -p"$DATABASE_PASS" -e "FLUSH PRIVILEGES"
+
+sudo systemctl restart mariadb
+
+sudo apt update 
+sudo apt -y install php php-{mysql,curl,gd,intl,pear,imap,memcache,pspell,tidy,xmlrpc,mbstring,gmp,json,xml,fpm}
+
+
+sudo git clone --recursive https://github.com/phpipam/phpipam.git /var/www/html/phpipam
+
+cd /var/www/html/phpipam
+
+cp config.dist.php config.php
+
+#edited config.php file
+sudo sed -i 's/phpipamadmin/Pass-2024/' config.php
+sudo apt -y install apache2
+
+sudo a2dissite 000-default.conf
+sudo systemctl reload apache2
+sudo a2enmod rewrite
+sudo systemctl restart apache2
+
+sudo apt -y install libapache2-mod-php php-curl php-xmlrpc php-intl php-gd
+
+cd /etc/apache2/sites-available/
+
+cat <<EOT > phpipam.conf
+#add below
+<VirtualHost *:80>
+    ServerAdmin admin@example.com
+    DocumentRoot "/var/www/html/phpipam"
+    ServerName ipam.example.com
+    ServerAlias www.ipam.example.com
+    <Directory "/var/www/html/phpipam">
+        Options Indexes FollowSymLinks
+        AllowOverride All
+        Require all granted
+    </Directory>
+    ErrorLog "/var/log/apache2/phpipam-error_log"
+    CustomLog "/var/log/apache2/phpipam-access_log" combined
+</VirtualHost>
+
+EOT
+
+chown -R www-data:www-data /var/www/html
+sudo a2ensite phpipam
+sudo systemctl reload apache2
+
+sudo systemctl restart apache2
+
+
+#localhost:80 in browser
+#New phpipam installation“
+
+#automatic installation
+
+
+
---
+++ zabbix final.sh
@@ -0,0 +1,55 @@
+DATABASE_PASS='Pass-2024'
+sudo apt update
+sud apt install -y curl vim
+
+
+wget https://repo.zabbix.com/zabbix/6.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_6.0-4+ubuntu22.04_all.deb
+dpkg -i zabbix-release_6.0-4+ubuntu22.04_all.deb
+apt update
+
+sudo apt install zabbix-server-mysql zabbix-frontend-php zabbix-apache-conf zabbix-sql-scripts zabbix-agent -y
+
+
+sudo apt install mysql-server -y
+
+sudo mysqladmin -u root password "$DATABASE_PASS"
+sudo mysql -u root -p"$DATABASE_PASS" -e "create database zabbix character set utf8mb4 collate utf8mb4_bin"
+sudo mysql -u root -p"$DATABASE_PASS" -e "create user zabbix@localhost identified by 'Pass-2024'"
+sudo mysql -u root -p"$DATABASE_PASS" -e "grant all privileges on zabbix.* to zabbix@localhost"
+sudo mysql -u root -p"$DATABASE_PASS" -e "set global log_bin_trust_function_creators = 1"
+
+echo ""
+echo "--------------------------------------------------------"
+echo ""
+echo "Zabbix server host is importing initial schema and data!!!"
+echo ""
+echo "--------------------------------------------------------"
+echo ""
+
+zcat /usr/share/zabbix-sql-scripts/mysql/server.sql.gz | mysql --default-character-set=utf8mb4 -uzabbix -pPass-2024 zabbix
+echo ""
+echo "--------------------------------------------------------"
+echo ""
+echo "Zabbix server host finished initial schema and data!!!"
+echo ""
+echo "--------------------------------------------------------"
+echo ""
+sleep 2s
+
+sudo mysql -u root -p"$DATABASE_PASS" -e "set global log_bin_trust_function_creators = 0"
+
+sudo sed -i 's/=password/=Pass-2024/' /etc/zabbix/zabbix_server.conf
+
+sudo systemctl restart zabbix-server zabbix-agent apache2
+sudo systemctl enable zabbix-server zabbix-agent apache2
+sudo service zabbix-server status
+IP_Address=$(hostname -I)
+echo ""
+echo "--------------------------------------------------------"
+echo ""
+echo "Zabbix server installation completed and you can accesss at http://$IP_Address/zabbix"
+echo "Default username: Admin"
+echo "Default password: zabbix"
+echo ""
+echo "--------------------------------------------------------"
+echo ""
